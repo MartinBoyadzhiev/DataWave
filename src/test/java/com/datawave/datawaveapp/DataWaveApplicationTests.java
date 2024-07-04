d7f65f0833001aa1651411ea6f3ec1a3cdd699d4
MartinBoyadzhiev <martin.boqdjiev00@gmail.com>
2024-07-04 20:40:21 +0300

    Spring Boot first commit.

---
+++ src/test/java/com/datawave/datawaveapp/DataWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datawave.datawaveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataWaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
