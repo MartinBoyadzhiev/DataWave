5607199fefb1b71658a2abf515fc5a09d0b8592a
MartinBoyadzhiev <martin.boqdjiev00@gmail.com>
2024-07-28 22:05:31 +0300

    Add table creation feature.

--- src/main/java/com/datawave/datawaveapp/config/securityConfig/JwtConstant.java
+++ src/main/java/com/datawave/datawaveapp/config/securityConfig/JwtConstant.java
@@ -2,7 +2,7 @@ package com.datawave.datawaveapp.config.securityConfig;
 
 public class JwtConstant {
 //    TODO: Change the secret key to a more secure value
-    public static final String SECRET_KEY = "wpembytrwcvnryxksdbqwjebruyGHyudqgwveytrtrCSnwifoesarjbwe";
+    public static final String SECRET_KEY = "";
 
     public static final String JWT_HEADER = "Authorization";
 }
